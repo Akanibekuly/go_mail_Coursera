b9b5832ea0a5fdf2f3c77444b5210c9e01ef61d8
Akanibekuly <a.kanibekuly@gmail.com>
2020-10-24 19:30:32 +0600

    feat: uniq programm

---
+++ unikalizacya/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+func uniq(input io.Reader, output io.Writer) error {
+	in := bufio.NewScanner(input)
+	var prev string
+	for in.Scan() {
+		txt := in.Text()
+
+		if txt == prev {
+			continue
+		}
+		if txt < prev {
+			return fmt.Errorf("file not sorted")
+		}
+		prev = txt
+		fmt.Fprintln(output, txt)
+	}
+	return nil
+}
+
+func main() {
+
+}
