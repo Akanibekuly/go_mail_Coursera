46b373620c50195cec8363790b7e90cbf61bc27d
Akanibekuly <a.kanibekuly@gmail.com>
2020-10-25 13:52:13 +0600

    docs: add materials

--- unikalizacya/main_test.go
+++ unikalizacya/main_test.go
@@ -14,12 +14,36 @@ var testOk = `1
 4
 5
 `
+var testOkResult = `1
+2
+3
+4
+5
+`
 
 func TestOK(t *testing.T) {
 	in := bufio.NewReader(strings.NewReader(testOk))
 	out := new(bytes.Buffer)
 	err := Uniq(in, out)
 	if err != nil {
-		t.Errorf("Test for OK Failed")
+		t.Errorf("Test for OK Failed - error")
+	}
+
+	result := out.String()
+	if result != testOkResult {
+		t.Errorf("Test for OK Failed - results not match: %v %v", result, testOkResult)
+	}
+}
+
+var testFail = `1
+2
+1`
+
+func TestError(t *testing.T) {
+	in := bufio.NewReader(strings.NewReader(testFail))
+	out := new(bytes.Buffer)
+	err := Uniq(in, out)
+	if err != nil {
+		t.Errorf("Test for Error Failed - error %v", err)
 	}
 }
