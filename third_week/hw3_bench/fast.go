aed805cc47638bb39a1619b9512c43cf5fa040e8
Akanibekuly <a.kanibekuly@gmail.com>
2020-11-09 02:45:18 +0600

    ge

--- third_week/hw3_bench/fast.go
+++ third_week/hw3_bench/fast.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"strings"
 	"sync"
+
+	"github.com/mailru/easyjson"
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
 )
 
 // const filePath string = "./data/users.txt"
@@ -19,12 +22,8 @@ import (
 
 type Data struct {
 	Browsers []string `json:"browsers"`
-	Company  string   `json:"-"`
-	Country  string   `json:"-"`
 	Email    string   `json:"email"`
-	Job      string   `json:"-"`
 	Name     string   `json:"name"`
-	Phone    string   `json:"-"`
 }
 
 var dataPool = sync.Pool{
@@ -55,7 +54,7 @@ func FastSearch(out io.Writer) {
 		}
 
 		d := dataPool.Get().(*Data)
-		err := json.Unmarshal(data, d)
+		err := easyjson.Unmarshal(data, d)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -90,4 +89,132 @@ func FastSearch(out io.Writer) {
 
 	// fmt.Fprintln(out, "")
 	fmt.Fprintln(out, "\nTotal unique browsers", len(seenBrowsers))
+}
+
+func easyjson3486653aDecodeHw3BenchTemp(in *jlexer.Lexer, out *Data) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "email":
+			out.Email = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson3486653aEncodeHw3BenchTemp(out *jwriter.Writer, in Data) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"browsers\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"email\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"name\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Name))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v Data) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson3486653aEncodeHw3BenchTemp(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v Data) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson3486653aEncodeHw3BenchTemp(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *Data) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson3486653aDecodeHw3BenchTemp(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *Data) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson3486653aDecodeHw3BenchTemp(l, v)
 }
