cd2434e79020360e70460dcd9764835a1c4f3083
Akanibekuly <a.kanibekuly@gmail.com>
2021-08-29 03:01:01 +0600

    feat: init funct

--- second_course/second_week/hw6_db_explorer/db_explorer.go
+++ second_course/second_week/hw6_db_explorer/db_explorer.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type Handler struct {
-	DB *sql.DB
+	DB     *sql.DB
+	Tables map[string]*Table
 }
 
 type Response struct {
@@ -19,37 +21,120 @@ type Response struct {
 	Response interface{} `json:"response,omitempty"`
 }
 
-type Tables struct {
+type Table struct {
+	Name   string
+	Fields []Field
 }
 
-type Fields struct {
-	Name string
+type Field struct {
+	Name            string
+	Type            string
+	IsPrimary       bool
+	IsNullable      bool
+	IsAutoincrement bool
 }
 
 func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 	h := Handler{}
 	h.DB = db
+	h.Tables = make(map[string]*Table)
+
+	if err := h.getAllTabels(); err != nil {
+		return nil, err
+	}
+
+	if err := h.getTableFields(); err != nil {
+		return nil, err
+	}
+
 	return &h, nil
 }
 
-func (h *Handler) handleTables(w http.ResponseWriter, r *http.Request) {
-	query := "SHOW TABLES;"
-	rows, err := h.DB.Query(query)
+func (h *Handler) getAllTabels() error {
+	rows, err := h.DB.Query("SHOW TABLES")
 	if err != nil {
-		panic(err)
+		log.Printf("error with query all tables info: %s\n", err)
+		return err
 	}
-	defer rows.Close()
-
-	var tables []string
-
 	for rows.Next() {
-		var table string
-		err = rows.Scan(&table)
+		var name string
+		err = rows.Scan(&name)
 		if err != nil {
-			panic(err)
+			log.Printf("scanning error with query all tables info: %s\n", err)
+			return err
 		}
-		tables = append(tables, table)
+
+		h.Tables[name] = &Table{
+			Name: name,
+		}
 	}
+
+	return nil
+}
+
+func (h *Handler) getTableFields() error {
+	t := func(i string) string { // is func that converts type of sql type name into golang type name
+		if strings.HasPrefix(i, "int") {
+			return "int"
+		} else if strings.HasPrefix(i, "varchar") || i == "text" {
+			return "string"
+		} else {
+			panic("unsupported field type")
+		}
+	} // end of t
+
+	for _, table := range h.Tables {
+		query := fmt.Sprintf("SHOW FULL COLUMNS FROM %s", table.Name)
+		stm, err := h.DB.Prepare(query) // we need to prepare cause of sql injection cases
+		if err != nil {
+			log.Printf("prepare error show full columns: %s\n", err)
+			return err
+		}
+		defer stm.Close() // always close statements
+
+		rows, err := stm.Query()
+		if err != nil {
+			log.Printf("query error show full columns: %s\n", err)
+			return err
+		}
+		defer rows.Close() // and close rows
+
+		var w interface{}
+		var isNullable, isPrimary, isAutoIncrement string
+		for rows.Next() {
+			var f Field
+			err := rows.Scan(&f.Name, &f.Type, &w, &isNullable, &isPrimary, &w, &isAutoIncrement, &w, &w)
+			if err != nil {
+				log.Printf("scan error show full columns %s: %s\n", table.Name, err)
+				return err
+			}
+			f.Type = t(f.Type)
+			if isNullable == "YES" {
+				f.IsNullable = true
+			}
+			if isPrimary == "PRI" {
+				f.IsPrimary = true
+			}
+			if isAutoIncrement == "auto_increment" {
+				f.IsAutoincrement = true
+			}
+
+			table.Fields = append(table.Fields, f)
+		}
+	}
+
+	return nil
+}
+
+func (h *Handler) handleTables(w http.ResponseWriter, r *http.Request) {
+	tables := make([]string, 0, len(h.Tables))
+
+	// will return all the tables that we got when we start the server
+	for name, _ := range h.Tables {
+		tables = append(tables, name)
+	}
+
+	sort.Strings(tables)
 
 	j, err := json.Marshal(Response{"", map[string]interface{}{"tables": tables}})
 	if err != nil {
@@ -62,7 +147,7 @@ type getQuery struct {
 	tableName string
 	limit     int
 	offset    int
-	id        int
+	id        *int
 }
 
 func parseQuery(r *http.Request) *getQuery {
@@ -77,7 +162,8 @@ func parseQuery(r *http.Request) *getQuery {
 	var err error
 	q := r.URL.Query()
 	if strID, ok := q["id"]; ok {
-		query.id, err = strconv.Atoi(strID[0])
+		id, err := strconv.Atoi(strID[0])
+		query.id = &id
 		if err != nil {
 			log.Printf("query error: id should be int: %s\n", err)
 			return nil
@@ -115,24 +201,29 @@ func (h *Handler) handleShow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	rows, err := h.DB.Query("SHOW FULL COLUMNS FROM " + q.tableName)
-	if err != nil {
-		if strings.Contains(err.Error(), "doesn't exist") {
-			j, err := json.Marshal(map[string]interface{}{"error": "unknown table"})
-			if err != nil {
-				http.Error(w, "internal server error", http.StatusInternalServerError)
-				return
-			}
-			w.WriteHeader(http.StatusNotFound)
-			w.Write(j)
+	table, ok := h.Tables[q.tableName]
+	if !ok {
+		j, err := json.Marshal(map[string]interface{}{"error": "unknown table"})
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
 			return
 		}
-		http.Error(w, "internal server error", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(j)
 		return
 	}
 
-	for rows.Next() {
-
+	if q.id != nil { // if id exists in the query
+		var primaryKey string
+		for _, f := range table.Fields {
+			if f.IsPrimary {
+				primaryKey = f.Name
+				break
+			}
+		}
+		
+		query := fmt.Sprintf("SELECT * FROM %s WHERE %s=?", q.tableName, primaryKey)
+		h.DB.QueryRow(query, *q.id)
 	}
 }
 
